ee4d6fc49e9f6982b0a578ca30cc90acbc70f51a
mohamed ayad <126675322+mohamed-hussein-ayad@users.noreply.github.com>
2024-08-21 16:06:23 +0300

    hay

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
     <>
       <Player />
       <div id="challenges">
-        <TimerChallenge title="Easy" targetTime={}></TimerChallenge>
-        <TimerChallenge title="Not easy" targetTime={}></TimerChallenge>
-        <TimerChallenge title="Getting tough" targetTime={}></TimerChallenge>
-        <TimerChallenge title="Pros only" targetTime={}></TimerChallenge>
+        <TimerChallenge title="Easy" targetTime={1}></TimerChallenge>
+        <TimerChallenge title="Not easy" targetTime={5}></TimerChallenge>
+        <TimerChallenge title="Getting tough" targetTime={10}></TimerChallenge>
+        <TimerChallenge title="Pros only" targetTime={15}></TimerChallenge>
       </div>
     </>
   );
